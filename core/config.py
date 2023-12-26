0d1579f436462eab45e8eea8b0f6da2003933286
Julio Orellana <coppro.cman@proton.me>
2023-12-27 00:03:18 +0100

    Update Settings Validation

--- core/config.py
+++ core/config.py
@@ -22,6 +22,14 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
     
+    @validator("CLIENT_DOMAINS", pre=True)
+    def assemble_client_domains(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+    
     class Config:
         env_file = "./.env"
 
